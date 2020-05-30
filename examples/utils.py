674518cb338ba7811501f8ccccd21a05892831e3
Ackerley Tng <ackerleytng@gmail.com>
2020-05-30 18:21:52 +0800

    Improve dumping util

--- examples/utils.py
+++ examples/utils.py
@@ -3,10 +3,17 @@ import urllib3
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
 
-def dump(response):
+def _dump(r):
+    print(f"  {r.status_code} {r.request.method} {r.url}")
+
+def dump(response, full=False):
+    if full:
+        print(f"response.text:")
+        print(response.text)
+
     print(f"response.status_code: {response.status_code}")
     print("response.history:")
     for r in response.history:
-        print("  " + r.url)
+        _dump(r)
 
-    print("  " + response.url)
+    _dump(response)
